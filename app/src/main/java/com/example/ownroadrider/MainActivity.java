837aef792e0e5da8ed31f3572f9a6de8976f6ec3
lemonmungchi <khc112353@gmail.com>
2023-05-03 18:02:10 +0900

    네이버맵추가
    네이버맵추가

--- app/src/main/java/com/example/ownroadrider/MainActivity.java
+++ app/src/main/java/com/example/ownroadrider/MainActivity.java
@@ -21,30 +21,6 @@ import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void getHashKey(){
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
-            }
-        }
-    }
-
-
-    private ImageButton advancedSearch;  //하단 바 검색 버튼
-    private ImageButton map;    //지도 버튼
 
     private EditText startpoint;
     private EditText endpoint;
@@ -53,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton homeButton_m;
     private ImageButton categoryButton_m;
-    private ImageButton mapButton_m;
+    private ImageButton mapButton;
     private ImageButton detailSearchButton_m;
     private ImageButton mypageButton_m;
 
@@ -66,9 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getHashKey();
 
-        advancedSearch = findViewById(R.id.detailSearchButton);
         startpoint=findViewById(R.id.startPointSearch);
         endpoint=findViewById(R.id.finishPointSearch);
         pathSearchButton=findViewById(R.id.pathSearchButton);
@@ -93,9 +67,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //맵
-        mapButton_m=findViewById(R.id.mapButton);
+        mapButton=findViewById(R.id.mapButton);
 
-        mapButton_m.setOnClickListener(new View.OnClickListener() {
+        mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Map_view.class);
@@ -127,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent =new Intent(MainActivity.this, MapSearchActivity.class);
+                Intent intent =new Intent(MainActivity.this, Map_search.class);
                 intent.putExtra("start",startpoint.getText().toString());
                 intent.putExtra("end",endpoint.getText().toString());
                 startActivity(intent);
---
+++ app/src/main/java/com/example/ownroadrider/Map_search.java
@@ -0,0 +1,296 @@
+package com.example.ownroadrider;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+
+public class Map_search extends AppCompatActivity implements OnMapReadyCallback {
+    private MapView mapView;
+    int[][] matrixGN = new int[][]{
+            {0, 24 - 12, 39, 56, Integer.MAX_VALUE},
+            {24 - 12, 0, Integer.MAX_VALUE, 74 - 37, 85 - 38},
+            {39, Integer.MAX_VALUE, 0, 85, 96},
+            {56, 74 - 37, 85, 0, 14},
+            {Integer.MAX_VALUE, 85 - 38, 96, 14, 0}
+    };
+
+    double[][] region_position = {
+            {35.2371, 128.6066},         //창원
+            {35.1805, 128.1087},         //진주
+            {35.0903, 128.0705},         //사천
+            {34.8918, 128.6206},         //거제
+            {34.8953, 127.8828},         //남해
+            {35.1407, 128.484}           //마산
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_search);
+
+        mapView = findViewById(R.id.map_search_view);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {                                        //맵객체 매서드 사용시 여기서 작성
+
+        Intent intent = getIntent();
+        String st = intent.getStringExtra("start");          //출발지 받아오기
+        String ed = intent.getStringExtra("end");             //목적지 받아오기
+        Dijkstra dj=new Dijkstra(5, matrixGN);
+        String[]rot =new String[3];
+        Marker marker_m = new Marker();             //경유지 마커
+        Marker marker_e = new Marker();             //도착지 마커
+
+        switch(st){
+            case"진주":
+                switch (ed){
+                    case"사천":
+                        rot=dj.algorithm("진주","사천");
+                        break;
+                    case"창원":
+                        rot=dj.algorithm("진주","창원");
+                        break;
+                    case"거제":
+                        rot=dj.algorithm("진주","거제");
+                        break;
+                    case"남해":
+                        rot=dj.algorithm("진주","남해");
+                        break;
+                    case"마산":
+                        rot=dj.algorithm("진주","마산");
+                        break;
+                }
+                break;
+            case"사천":
+                switch (ed){
+                    case"진주":
+                        rot=dj.algorithm("사천","진주");
+                        break;
+                    case"창원":
+                        rot=dj.algorithm("사천","창원");
+                        break;
+                    case"거제":
+                        rot=dj.algorithm("사천","거제");
+                        break;
+                    case"남해":
+                        rot=dj.algorithm("사천","남해");
+                        break;
+                    case"마산":
+                        rot=dj.algorithm("사천","마산");
+                        break;
+                }
+                break;
+            case"창원":
+                switch (ed){
+                    case"진주":
+                        rot=dj.algorithm("창원","진주");
+                        break;
+                    case"사천":
+                        rot=dj.algorithm("창원","사천");
+                        break;
+                    case"거제":
+                        rot=dj.algorithm("창원","거제");
+                        break;
+                    case"남해":
+                        rot=dj.algorithm("창원","남해");
+                        break;
+                    case"마산":
+                        rot=dj.algorithm("창원","마산");
+                        break;
+                }
+                break;
+            case"거제":
+                switch (ed){
+                    case"진주":
+                        rot=dj.algorithm("거제","진주");
+                        break;
+                    case"사천":
+                        rot=dj.algorithm("거제","사천");
+                        break;
+                    case"창원":
+                        rot=dj.algorithm("거제","창원");
+                        break;
+                    case"남해":
+                        rot=dj.algorithm("거제","남해");
+                        break;
+                    case"마산":
+                        rot=dj.algorithm("거제","마산");
+                        break;
+                }
+                break;
+            case"남해":
+                switch (ed){
+                    case"진주":
+                        rot=dj.algorithm("남해","진주");
+                        break;
+                    case"사천":
+                        rot=dj.algorithm("남해","사천");
+                        break;
+                    case"창원":
+                        rot=dj.algorithm("남해","창원");
+                        break;
+                    case"거제":
+                        rot=dj.algorithm("남해","거제");
+                        break;
+                    case"마산":
+                        rot=dj.algorithm("남해","마산");
+                        break;
+                }
+                break;
+            case"마산":
+                switch (ed){
+                    case"진주":
+                        rot=dj.algorithm("마산","진주");
+                        break;
+                    case"사천":
+                        rot=dj.algorithm("마산","사천");
+                        break;
+                    case"창원":
+                        rot=dj.algorithm("마산","창원");
+                        break;
+                    case"거제":
+                        rot=dj.algorithm("마산","거제");
+                        break;
+                    case"남해":
+                        rot=dj.algorithm("마산","남해");
+                        break;
+                }
+                break;
+        }
+        switch (st) {                                     //시작점 찍기
+            case "진주":
+                Marker marker = new Marker();
+                marker.setPosition(new LatLng(35.1805, 128.1087));
+                marker.setMap(naverMap);
+                marker.setWidth(80);
+                marker.setHeight(100);
+                break;
+        }
+
+        switch (rot[1]) {                                   //경유지찍기
+            case "사천":
+                marker_m.setPosition(new LatLng(region_position[2][0], region_position[2][1]));
+                marker_m.setMap(naverMap);
+                marker_m.setWidth(80);
+                marker_m.setHeight(100);
+                break;
+            case "창원":
+                marker_m.setPosition(new LatLng(region_position[0][0], region_position[0][1]));
+                marker_m.setMap(naverMap);
+                marker_m.setWidth(80);
+                marker_m.setHeight(100);
+                break;
+            case "거제":
+                marker_m.setPosition(new LatLng(region_position[3][0], region_position[3][1]));
+                marker_m.setMap(naverMap);
+                marker_m.setWidth(80);
+                marker_m.setHeight(100);
+                break;
+            case "남해":
+                marker_m.setPosition(new LatLng(region_position[4][0], region_position[4][1]));
+                marker_m.setMap(naverMap);
+                marker_m.setWidth(80);
+                marker_m.setHeight(100);
+                break;
+            case "마산":
+                marker_m.setPosition(new LatLng(region_position[5][0], region_position[5][1]));
+                marker_m.setMap(naverMap);
+                marker_m.setWidth(80);
+                marker_m.setHeight(100);
+                break;
+        }
+
+
+        switch (ed) {                                                   //도착지
+            case "사천":
+                marker_e.setPosition(new LatLng(region_position[2][0], region_position[2][1]));
+                marker_e.setMap(naverMap);
+                marker_e.setWidth(80);
+                marker_e.setHeight(100);
+                break;
+            case "창원":
+                marker_e.setPosition(new LatLng(region_position[0][0], region_position[0][1]));
+                marker_e.setMap(naverMap);
+                marker_e.setWidth(80);
+                marker_e.setHeight(100);
+                break;
+            case "거제":
+                marker_e.setPosition(new LatLng(region_position[3][0], region_position[3][1]));
+                marker_e.setMap(naverMap);
+                marker_e.setWidth(80);
+                marker_e.setHeight(100);
+                break;
+            case "남해":
+                marker_e.setPosition(new LatLng(region_position[4][0], region_position[4][1]));
+                marker_e.setMap(naverMap);
+                marker_e.setWidth(80);
+                marker_e.setHeight(100);
+                break;
+            case "마산":
+                marker_e.setPosition(new LatLng(region_position[5][0], region_position[5][1]));
+                marker_e.setMap(naverMap);
+                marker_e.setWidth(80);
+                marker_e.setHeight(100);
+                break;
+        }
+        naverMap.setCameraPosition(new CameraPosition(new LatLng(35.2519,128.3094),6));
+    }
+}
