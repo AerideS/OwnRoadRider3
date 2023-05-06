62bb7e9342e3c38835ce6f017a8f7a98756b471b
GiveMeSome <kimsman1031@naver.com>
2023-05-06 20:48:23 +0900

    [ADD] 추천일정 카드뷰 UI 추가 (-민섭)

---
+++ app/src/main/java/com/example/ownroadrider/RecommendedScheduleActivity.java
@@ -0,0 +1,14 @@
+package com.example.ownroadrider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecommendedScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommended_schedule);
+    }
+}
